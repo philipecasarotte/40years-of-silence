f93238c694fde6601947efce45b4f64e53abfc6a
Philipe Casarotte <philipe@philipe.(none)>
2008-11-17 14:33:13 -0200

    layotus changes for css and images of mernu

--- public/javascripts/page/trailer.js
+++ public/javascripts/page/trailer.js
@@ -1,14 +1,16 @@
 $(function() {
       $('#videoPlayer').flashembed({
         src: '/swf/player.swf',
-        width: 482,
-        height: 364+28
+        //width: 482,
+        //height: 364+28
+        width: 720,
+        height: 480+28
       }, {
         config: {
           videoFile: '/flv/trailer.flv',
           splashImageFile: '/images/trailer_splash.jpg',
           controlBarBackground: 0x000000,
-          initialScale: 'orig',
+          initialScale: 'fill',
           initialVolumePercentage: 100,
           loop: false,
           menuItems: [true, true, true, true, true, false],
