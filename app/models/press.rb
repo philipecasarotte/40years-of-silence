0dd7900526f867fe218ada75e491f111ed667a10
Philipe Casarotte <philipecasarotte@Philipe-Casarottes-Computer.local>
2008-12-29 17:38:09 -0200

    add layoutr for presses and downloads

--- app/models/press.rb
+++ app/models/press.rb
@@ -1,6 +1,6 @@
 class Press < ActiveRecord::Base                    
   has_attached_file :cover,
-                    :styles => {:image => "250x180#", :thumb => "76x58#"},
+                    :styles => {:image => "200x180", :thumb => "76x58#"},
                     :path => ":rails_root/public/uploads/:class/:id/:style_:basename.:extension",
                     :url => "/uploads/:class/:id/:style_:basename.:extension"
 end
