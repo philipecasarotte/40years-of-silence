389dc02f76fa8195f4c580ab871749c47c96a67b
Philipe Casarotte <philipecasarotte@Philipe-Casarottes-Computer.local>
2008-12-30 16:19:57 -0200

    add validatiuon for pdf file on downloads

--- app/models/download.rb
+++ app/models/download.rb
@@ -1,5 +1,7 @@
 class Download < ActiveRecord::Base
-   has_attached_file :pdf,
-                     :path => ":rails_root/public/uploads/:class/:id/:style_:basename.:extension",
-                     :url => "/uploads/:class/:id/:style_:basename.:extension"
+  has_attached_file :pdf,
+                    :path => ":rails_root/public/uploads/:class/:id/:style_:basename.:extension",
+                    :url => "/uploads/:class/:id/:style_:basename.:extension"
+  validates_attachment_presence :pdf
+  validates_attachment_content_type :pdf, :content_type => 'application/pdf'
 end
