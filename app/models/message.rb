a7e76ff8c97f1938d8e7f72d7a423028506b7917
Philipe Casarotte <philipecasarotte@Philipe-Casarottes-Computer.local>
2008-12-30 15:37:13 -0200

    add css for psot and comments on admin

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ActiveRecord::Base
   validates_presence_of :title, :if=>Proc.new { |m| m.parent_id.nil? }
   
   named_scope :active, :conditions => {:active => true}
-  named_scope :pending, :conditions => {:active => false}, :order => 'parent_id'
+  named_scope :pending, :conditions => {:active => false}, :order => :parent_id
   
   def approve!
     update_attribute(:active, true)
