dedca9a028771afd23c79f3af5e95d626c2e3ba2
Philipe Casarotte <philipecasarotte@Philipe-Casarottes-Computer.local>
2009-01-06 18:04:50 -0200

    change photos images size

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ActiveRecord::Base
   belongs_to :album, :counter_cache=>true
   acts_as_list :scope=>'album_id'
   
-  has_attached_file :image, :styles => { :medium => "417x314>", 
-                                         :thumb => "76x57#",
-                                         :big => "410x315>" }
+  has_attached_file :image, :styles => { :medium => "417x314>",
+                                         :thumb => "61x81 #",
+                                         :big => "482x3655>" }
 end
