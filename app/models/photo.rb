c6890b6ff15803b6d852df0e30e0c0b801dcb8ca
Philipe Casarotte <philipe@dburnsdesign.com>
2009-01-21 10:34:04 -0200

    change gallery thumb size add menu background black 100

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,6 +3,6 @@ class Photo < ActiveRecord::Base
   acts_as_list :scope=>'album_id'
   
   has_attached_file :image, :styles => { :medium => "417x314>",
-                                         :thumb => "61x81 #",
+                                         :thumb => "81x61 #",
                                          :big => "482x3655>" }
 end
