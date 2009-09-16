29e512e71b2b75cbb71ffa6cc2ea8a858c789c45
Philipe Casarotte <philipe@dburnsdesign.com>
2009-09-16 17:42:02 -0300

    add deploy and change image defautl path

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,8 @@ class Photo < ActiveRecord::Base
   belongs_to :album, :counter_cache => true
   acts_as_list :scope => 'album_id'
   
-  has_attached_file :image, :styles => { :medium => "417x314>",
-                                         :thumb => "81x61 #",
-                                         :big => "482x3655>" }
+  has_attached_file :image,
+                    :styles => { :medium => "417x314>", :thumb => "81x61 #", :big => "482x3655>" },
+                    :path => ":rails_root/public/uploads/images/:id/:style_:basename.:extension",
+                    :url => "/uploads/images/:id/:style_:basename.:extension"
 end
